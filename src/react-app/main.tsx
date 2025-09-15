349e503a67a02794771ffa1b8bed642bda9148d4
alex-yeung-q <alex.yeung.qbe@gmail.com>
2025-09-15 01:11:18 +0000

    Added dummy context

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -6,10 +6,18 @@ import App from "./App.tsx";
 
 const LD_CLIENT_ID = "68c2546fae804509aa5aa699";
 
+const DUMMY_CONTEXT = {
+  "kind": "user",
+  "key": "user-key-123abc",
+  "name": "Anna",
+  "email": "anna@globalhealthexample.com",
+  "jobFunction": "doctor"
+}
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <LDProvider clientSideID={LD_CLIENT_ID}>
+    <LDProvider clientSideID={LD_CLIENT_ID} context={DUMMY_CONTEXT} >
       <App />
     </LDProvider>
   </StrictMode>,
